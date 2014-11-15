26b3d81e0ec98f3d52ad76af7aadfcf95287ae9b
Hugo.fxy <hugo.fxy@gmail.com>
2014-11-15 23:14:36 +0100

    comment out Sleep

--- convnet/main.cpp
+++ convnet/main.cpp
@@ -43,7 +43,7 @@ int main(){
 
 	n.train(test_x, test_y, 10000);
     int test_sample_count = 10;
-    Sleep(1000);
+    //Sleep(1000);
     printf("Testing with %d samples:\n", test_sample_count);
     const clock_t begin_time = clock();
     n.test(test_x, test_y, test_sample_count, 10);
